6dfb2445368752414d9ff45e00c07f8e7753b9eb
Yusuf <yusufagungh02@gmail.com>
2022-10-05 08:55:21 +0700

    Tugas Sesi 6

---
+++ Perulangan/challenge.js
@@ -0,0 +1,25 @@
+console.log("Challenge No. 1")
+var index = 1
+while(index <=20){
+    console.log("Halo, saya orang ke URUTAN ORANG:", index)
+    index++
+}
+
+console.log("Challenge No. 2")
+var index = 0
+while(index <=20){
+    if(index % 2 == 1)
+    console.log("Halo, saya orang ke URUTAN ORANG:", index)
+    
+    index++
+}
+
+// var huruf="OOOO "
+// console.log(huruf.repeat(4))
+ 
+// var index = 0
+// var huruf="O"
+// while(index < 100){
+//     console.log(huruf.repeat(100))
+//     index++
+// }
